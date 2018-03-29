ad61845c347d35c12c5e2193c05c0cdc966eb010
Ville Raustia <ville.raustia@hiq.fi>
2018-03-29 15:17:30 +0300

    Initial commit

---
+++ Frends.Community.StaticStorage.Tests/TaskTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace Frends.Community.StaticStorage.Tests
+{
+    [TestClass]
+    public class TaskTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            Task._storage.Clear();
+        }
+
+        [TestMethod]
+        public void Add_ShouldAddKeyValuePair()
+        {
+            Task.Add("key", 123, false);
+
+            Assert.IsTrue(Task._storage.ContainsKey("key"));
+            Assert.IsTrue(Task._storage.ContainsValue(123));
+        }
+
+        [TestMethod]
+        public void Add_ShouldOverwriteValue()
+        {
+            Task.Add("key", 123, false);
+            Task.Add("key", 12345, true);
+
+            Assert.AreEqual(12345, Task._storage["key"]);
+        }
+
+        [TestMethod]
+        public void Exists_ShouldReturnFalseIfKeyDoesNotExist()
+        {
+            Task.Add("key", 123, false);
+
+            Assert.IsFalse(Task.ContainsKey("keyThatDoesNotExist"));
+        }
+
+        [TestMethod]
+        public void Exists_ShouldReturnTrueIfKeyExists()
+        {
+            Task.Add("key", 123, false);
+
+            Assert.IsTrue(Task.ContainsKey("key"));
+        }
+
+        [TestMethod]
+        public void ToJToken_ShouldReturnDictionaryAsJToken()
+        {
+            Task.Add("key", 123, false);
+            JToken reference = JToken.Parse(@"{""key"":123 }");
+
+            JToken result = (JToken)Task.ToJToken();
+
+            Assert.AreEqual(reference.ToString(), result.ToString());
+        }
+    }
+}
---
+++ Frends.Community.StaticStorage/Task.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Frends.Community.StaticStorage.Tests")]
+namespace Frends.Community.StaticStorage
+{
+    public class Task
+    {
+        internal readonly static Dictionary<string, object> _storage = new Dictionary<string, object>();
+
+        /// <summary>
+        /// Adds a key-value-pair to the storage
+        /// </summary>
+        /// <param name="key">Key to identify added value</param>
+        /// <param name="value">Value to add</param>
+        /// <param name="overwrite">Should existing key-value be overwritten?</param>
+        public static bool Add(string key, object value, bool overwrite = false)
+        {
+            if (overwrite)
+                _storage[key] = value;
+            else
+                _storage.Add(key, value);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Returns value from storage
+        /// </summary>
+        /// <param name="key">Key to the value to return to</param>
+        /// <returns>object</returns>
+        public static object Get(string key)
+        {
+            return _storage[key];
+        }
+
+        /// <summary>
+        /// Removes key-value-pair from storage
+        /// </summary>
+        /// <param name="key">Key to remove</param>
+        public static bool Remove(string key)
+        {
+            _storage.Remove(key);
+            return true;
+        }
+
+        /// <summary>
+        /// Clears all storage keys and values
+        /// </summary>
+        public static bool Clear()
+        {
+            _storage.Clear();
+            return true;
+        }
+
+        /// <summary>
+        /// Checks if key exists in storage
+        /// </summary>
+        /// <param name="key">Key to check</param>
+        /// <returns>True if key exists, otherwise false</returns>
+        public static bool ContainsKey(string key)
+        {
+            return _storage.ContainsKey(key);
+        }
+
+        /// <summary>
+        /// Returns storage as JToken
+        /// </summary>
+        /// <returns>JToken</returns>
+        public static JToken ToJToken()
+        {
+            return JToken.FromObject(_storage);
+        }
+    }
+}
